67706aa5990b3c2c2f97ac0af541add00f157d3a
Kyra McCandless <mail@kyramccandless.com>
2025-10-05 21:36:09 -0400

    Exploring css with team roster

--- dev-roster-website/src/App.jsx
+++ dev-roster-website/src/App.jsx
@@ -21,6 +21,18 @@ function App() {
           <li>Test Developer Name</li>
           <li>Test Developer Name</li>
         </ul>
+        <h2 className="lex-zero-waste">Lexington Zero Waste</h2>
+        <ul>
+          <button className="dev-button">
+            <p className="kyra-mccandless">Kyra McCandless</p>
+          </button>
+        </ul>
+        <h2 className="add-space-above">This is my experimental button!</h2>
+          <ul>
+            <li>Changing background color, outline offset, and border radius</li>
+            <li>Hover and clicking effects</li>
+            <li>Fun fonts!</li>
+          </ul>
       </div>
     </>
   )
